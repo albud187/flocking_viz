f9b903cca763d424f208b48a6370ca1e4a70551d
albud187 <alfab@MSI.localdomain>
2024-09-08 18:37:01 -0400

    refactor and consolidate matrix tools

--- src/flocking_utils/flocking_technique.h
+++ src/flocking_utils/flocking_technique.h
@@ -1,6 +1,13 @@
 #ifndef FLOCKING_H
 #define FLOCKING_H
 #include <Eigen/Dense>
+#include <cmath>
+#include "../multi_utils/mesh.h"
+
+
+Eigen::MatrixXd VelocityMatrix(std::vector<std::shared_ptr<Mesh>> input_objects);
+Eigen::MatrixXd PositionMatrix(std::vector<std::shared_ptr<Mesh>> input_objects);
+void update_velocities(Eigen::MatrixXd velocity_matrix, std::vector<std::shared_ptr<Mesh>> input_objects);
 
 
 
